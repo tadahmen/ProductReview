187d60e8408b7d6458a29581047e79113b60ca73
tadahmen <tadahmen@gmail.com>
2016-03-09 16:53:49 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,5 @@ Product.create([
 #   { product: radio1.id, name: "Jan", rating: 3, reviewText:"Prima" },
 #   { product: radio1.id, name: "Bert", rating: 4, reviewText:"Goed" },
 #   { product: radio2.id, name: "Ali", rating: 2, reviewText:"tja" },
-#   { product: radio2.id, name: "Miep", rating: 3, reviewText:"ok" },
+#   { product: radio3.id, name: "Miep", rating: 3, reviewText:"ok" },
 # ])
