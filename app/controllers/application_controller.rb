6b6e2e99a050678f97d2432f18bc31d683727403
tadahmen <tadahmen@gmail.com>
2016-03-11 17:01:36 +0100

    allowance experiment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
   def set_allowed
   	headers['Access-Control-Allow-Origin'] = '*'
-  	headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-  	headers['Access-Control-Request-Method'] = '*'
-  	headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  	# headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+  	# headers['Access-Control-Request-Method'] = '*'
+  	# headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
