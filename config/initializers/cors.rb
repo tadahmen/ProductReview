73702bd7e8393079ce292718d2e3d66471a3fe24
tadahmen <tadahmen@gmail.com>
2016-03-12 21:45:45 +0100

    removed allowances app contr

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class ProductReview::Application
    config.middleware.insert_before 0,  "Rack::Cors", debug: Rails.env.development?, logger: (-> { Rails.logger }) do
      allow do
-       origins 'http://tutor-isabel-44515.bitballoon.com', '127.0.0.1:3000'   #localhost:3000 
+       origins '*', '127.0.0.1:3000'   #*, 'localhost:3000', ip of front is: 104.130.205.125
          /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
 
        resource '/cors',
