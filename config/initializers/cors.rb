9d706e65bc8461871db40e2b89abfa7a355f1ce4
tadahmen <tadahmen@gmail.com>
2016-03-13 12:44:15 +0100

    changed ip in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class ProductReview::Application
    config.middleware.insert_before 0,  "Rack::Cors", debug: Rails.env.development?, logger: (-> { Rails.logger }) do
      allow do
-       origins '*', '127.0.0.1:3000'   #*, 'localhost:3000', ip of front is: 104.130.205.125
+       origins '*', '104.130.205.125'  # 'localhost:3000', '127.0.0.1:3000' 
          /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
 
        resource '/cors',
